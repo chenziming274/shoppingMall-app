53eb568a5301ab589f7a6bdb85c32f90efe3ce65
chenziming <805966791@qq.com>
2022-07-12 10:12:02 +0800

    移动练习项目适配以及tabBar静态完成

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="tabBar">
+      <TabBar></TabBar>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabBar";
+
+export default {
+  name: "App",
+  components: {
+    TabBar,
+  },
+};
+</script>
+
+<style scoped>
+ .tabBar {
+  position: fixed;
+  height: 49px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+ }
+</style>
---
+++ src/components/tabBar/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tabBar">
+    <div class="homePage">
+      <img class="img" src="../../assets/images/首页默认.svg" alt="">
+      <div class="tagging">首页</div>
+    </div>
+    <div class="product">
+      <img class="img" src="../../assets/images/产品默认.svg" alt="">
+      <div class="tagging">产品</div>
+    </div>
+    <div class="news">
+      <img class="img" src="../../assets/images/消息默认.svg" alt="">
+      <div class="tagging">消息</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tabBar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.tabBar {
+  border-top: 1px solid #F2F3F5;
+  height: 49px;
+  display: flex;
+  justify-content: space-around;
+}
+.img {
+  width: 20px;
+  height: 20px;
+  margin-top: 7px;
+}
+.tagging {
+  transform: translateX(-2px);
+  font-size: 12px;
+}
+</style>
